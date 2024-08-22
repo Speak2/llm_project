fc0e7e16dc1b905e3cd3b66475fedbbb6dd29ded
Nahid Mahamud <nahid.nm91@gmail.com>
2024-08-22 13:54:00 +0600

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-DJANGO_DATABASE_CONFIG = {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'notun_database',
-    'USER': 'postgres',
-    'PASSWORD': 'p@stgress',
-    'HOST': 'localhost',
-    'PORT': '5433',
-}

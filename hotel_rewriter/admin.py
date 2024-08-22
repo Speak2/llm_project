ef2b6a8023c7d7c39096dc483ee50564e0778e20
Nahid Mahamud <nahid.nm91@gmail.com>
2024-08-22 13:58:16 +0600

    update

--- hotel_rewriter/admin.py
+++ hotel_rewriter/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Property
+from .models import Property, PropertySummary
 
 
 @admin.register(Property)
@@ -25,3 +25,28 @@ class PropertyAdmin(admin.ModelAdmin):
             'classes': ('collapse',)
         }),
     )
+
+
+@admin.register(PropertySummary)
+class PropertySummaryAdmin(admin.ModelAdmin):
+    # class Media:
+    #     css = {
+    #         'all': ('css/custom_admin.css',)
+    #     }
+    list_display = ('property_id', 'title',
+                    'create_date', 'update_date')
+    list_display_links = ('title',)
+    ordering = ('property_id',)
+    list_filter = ('create_date', 'update_date')
+    search_fields = ('property_id', 'title', 'description')
+    readonly_fields = ('create_date', 'update_date')
+
+    # fieldsets = (
+    #     ('Property Details', {
+    #         'fields': ('property_id', 'title', 'description')
+    #     }),
+    #     ('Timestamps', {
+    #         'fields': ('create_date', 'update_date'),
+    #         'classes': ('collapse',)
+    #     }),
+    # )

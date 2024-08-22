5ce39ecee6542183ee79d37eaa9c7b73302044ab
Nahid Mahamud <nahid.nm91@gmail.com>
2024-08-22 13:38:57 +0600

    update

--- hotel_rewriter/admin.py
+++ hotel_rewriter/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Property
 
-# Register your models here.
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    # class Media:
+    #     css = {
+    #         'all': ('css/custom_admin.css',)
+    #     }
+    list_display = ('property_id', 'title',
+                    'create_date', 'update_date')
+    list_display_links = ('title',)
+    ordering = ('property_id',)
+    list_filter = ('create_date', 'update_date')
+    search_fields = ('property_id', 'title', 'description')
+    readonly_fields = ('create_date', 'update_date')
+
+    fieldsets = (
+        ('Property Details', {
+            'fields': ('property_id', 'title', 'description')
+        }),
+        ('Timestamps', {
+            'fields': ('create_date', 'update_date'),
+            'classes': ('collapse',)
+        }),
+    )
--- hotel_rewriter/management/commands/rewrite_properties.py
+++ hotel_rewriter/management/commands/rewrite_properties.py
@@ -1,5 +1,3 @@
-# core/management/commands/rewrite_properties.py
-
 from django.core.management.base import BaseCommand
 from hotel_rewriter.models import Property, PropertySummary
 import ollama
@@ -9,25 +7,30 @@ class Command(BaseCommand):
     help = 'Rewrites property titles and descriptions, and generates summaries using Ollama'
 
     def handle(self, *args, **options):
-        model = "llama2"  # You can change this to any model available in Ollama
+        model = "gemma:2b"  # Make sure this matches the model you have in Ollama
+
+        # Create an Ollama client with the correct host and port
+        ollama_client = ollama.Client(host="http://localhost:8001")
 
         properties = Property.objects.all()
         for property in properties:
             # Rewrite title
             title_prompt = f"Rewrite the following property title to make it more appealing: {property.title}"
-            title_response = ollama.generate(model=model, prompt=title_prompt)
+            title_response = ollama_client.generate(
+                model=model, prompt=title_prompt)
             property.title = title_response['response'].strip()
 
             # Rewrite description
             desc_prompt = f"Rewrite the following property description to make it more engaging and informative: {property.description}"
-            desc_response = ollama.generate(model=model, prompt=desc_prompt)
+            desc_response = ollama_client.generate(
+                model=model, prompt=desc_prompt)
             property.description = desc_response['response'].strip()
 
             property.save()
 
             # Generate summary
             summary_prompt = f"Summarize the following property information:\nTitle: {property.title}\nDescription: {property.description}\nAmenities: {', '.join(property.amenities.values_list('name', flat=True))}\nLocations: {', '.join(property.locations.values_list('name', flat=True))}"
-            summary_response = ollama.generate(
+            summary_response = ollama_client.generate(
                 model=model, prompt=summary_prompt)
 
             PropertySummary.objects.update_or_create(
